89828df313ffa6a9b604eb48e42438bef66c313d
Andrea Mininni <andrea.mininni@aulab.it>
2023-03-23 12:43:18 +0100

    modifica dopo fallimento test

--- test/division.test.js
+++ test/division.test.js
@@ -1,6 +1,6 @@
 import division from './../src/division.js'
 
-describe("subtraction", () => {
+describe("division", () => {
     it("should return 5 for 10 and 2", () => {
         expect(division(10, 2)).toBe(5);
     })
